27b692264d6516989f8b13cbb90b634d543306ef
Vicens Fayos <datracka@gmail.com>
2019-04-09 17:08:01 +0300

    move app to his own package

--- app.py
+++ app.py
@@ -1,27 +1,5 @@
-from flask import Flask
-from flask_graphql import GraphQLView
-import graphene
+from diysna import app
 
-
-class Query(graphene.ObjectType):
-    hello = graphene.String(description='A typical hello world')
-
-    def resolve_hello(self, info):
-        return 'World'
-
-
-schema = graphene.Schema(query=Query)
-
-app = Flask(__name__)
-
-app.add_url_rule(
-    '/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello to the World of Flask!'
-
-
-if __name__ == '__main__':
-    app.run()
+# Load this config object for development mode
+app.config.from_object('config.DevelopmentConfig')
+app.run()
