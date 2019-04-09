7712b53c0a262d5be012e75d00ec9c777908e2b6
Vicens Fayos <datracka@gmail.com>
2019-04-09 16:35:34 +0300

    add graphql and a set of helper debugging libraries

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask
+from flask_graphql import GraphQLView
+import graphene
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(description='A typical hello world')
+
+    def resolve_hello(self, info):
+        return 'World'
+
+
+schema = graphene.Schema(query=Query)
+
 app = Flask(__name__)
+
+app.add_url_rule(
+    '/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+
+
 @app.route('/')
 def hello_world():
- return 'Hello to the World of Flask!'
-if __name__ == '__main__':
- app.run()
+    return 'Hello to the World of Flask!'
 
+
+if __name__ == '__main__':
+    app.run()
